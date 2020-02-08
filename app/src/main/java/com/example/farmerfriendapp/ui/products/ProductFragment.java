4dce7fd49dbc0665e5ed3b3d394770e015677be3
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-02-09 01:23:31 +0530

    df

---
+++ app/src/main/java/com/example/farmerfriendapp/ui/products/ProductFragment.java
@@ -0,0 +1,13 @@
+package com.example.farmerfriendapp.ui.products;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProductFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
