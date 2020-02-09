4d4164db0b2d126732751e73160afc87767ae4d6
Kush Gosalia <gosaliakush@gmail.com>
2020-02-09 09:54:02 +0530

    Login

--- app/src/main/java/com/example/farmerfriendapp/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/farmerfriendapp/ui/login/LoginFragment.java
@@ -1,59 +1,58 @@
 package com.example.farmerfriendapp.ui.login;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.farmerfriendapp.MainActivity;
 import com.example.farmerfriendapp.R;
 
 
 public class LoginFragment extends Fragment {
     View rootView;
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+        rootView=inflater.inflate(R.layout.fragment_login,container,false);
+        Button click=(Button) rootView.findViewById(R.id.login_button);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        if (!CheckNetwork.isNetworkConnected(requireContext())) {
-            Toast.makeText(requireContext(), "No Internet Connection!!!", Toast.LENGTH_LONG).show();
-             rootView = inflater.inflate(R.layout.fragment_no_internet, container, false);
-            return rootView;
-        } else {
-            rootView = inflater.inflate(R.layout.fragment_login, container, false);
-            return rootView;
-        }
+                EditText user = (EditText) rootView.findViewById(R.id.user_login);
+                String name = user.getText().toString();
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setDataAndType(Uri.parse("smsto:"),"vnd.android-dir/mms-sms");
+                sendIntent.putExtra("sms_body", "Your password is");
+                sendIntent.putExtra("address", name);
+                startActivity(sendIntent);
+            }
+        });
+        return rootView;
+
+
     }
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-//        if (!CheckNetwork.isNetworkConnected(requireContext())) {
-//            Toast.makeText(requireContext(), "No Internet Connection!!!", Toast.LENGTH_LONG).show();
-//        } else {
-//            WebView login = view.findViewById(R.id.webview);
-//            login.loadUrl("https://www.google.com");
-//            WebSettings webSettings = login.getSettings();
-//            webSettings.setJavaScriptEnabled(true);
-
-        //}
-        EditText user=(EditText)rootView.findViewById(R.id.user_login);
-        String name=user.getText().toString();
-        EditText pass=(EditText)rootView.findViewById(R.id.pass_login);
-        String password=pass.getText().toString();
 
 
-  }
-}
+
+    }
+
 
