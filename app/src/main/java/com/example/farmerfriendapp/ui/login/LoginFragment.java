96e16c9f80230a69b107f7c5d1504eb7fb21c48c
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-02-09 05:40:46 +0530

    sdfg

--- app/src/main/java/com/example/farmerfriendapp/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/farmerfriendapp/ui/login/LoginFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ import com.example.farmerfriendapp.R;
 
 
 public class LoginFragment extends Fragment {
+    View rootView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,7 +25,7 @@ public class LoginFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View rootView;
+
 
         if (!CheckNetwork.isNetworkConnected(requireContext())) {
             Toast.makeText(requireContext(), "No Internet Connection!!!", Toast.LENGTH_LONG).show();
@@ -37,14 +39,21 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if (!CheckNetwork.isNetworkConnected(requireContext())) {
-            Toast.makeText(requireContext(), "No Internet Connection!!!", Toast.LENGTH_LONG).show();
-        } else {
-            WebView login = view.findViewById(R.id.webview);
-            login.loadUrl("https://www.google.com");
-            WebSettings webSettings = login.getSettings();
-            webSettings.setJavaScriptEnabled(true);
-        }
+//        if (!CheckNetwork.isNetworkConnected(requireContext())) {
+//            Toast.makeText(requireContext(), "No Internet Connection!!!", Toast.LENGTH_LONG).show();
+//        } else {
+//            WebView login = view.findViewById(R.id.webview);
+//            login.loadUrl("https://www.google.com");
+//            WebSettings webSettings = login.getSettings();
+//            webSettings.setJavaScriptEnabled(true);
+
+        //}
+        EditText user=(EditText)rootView.findViewById(R.id.user_login);
+        String name=user.getText().toString();
+        EditText pass=(EditText)rootView.findViewById(R.id.pass_login);
+        String password=pass.getText().toString();
+
+
   }
 }
 
