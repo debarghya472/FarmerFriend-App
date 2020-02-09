cf430a8df4adfb8e7d2fe378f3881da346b1fb67
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-02-09 10:00:13 +0530

    sca

---
+++ app/src/main/java/com/example/farmerfriendapp/ui/register/RegisterFragment.java
@@ -0,0 +1,26 @@
+package com.example.farmerfriendapp.ui.register;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.farmerfriendapp.R;
+
+public class RegisterFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootview=inflater.inflate(R.layout.fragment_register,container,false);
+        return rootview;
+    }
+}
